1bc2e3d80f2ad6093ccfb319ee9ce5d054002789
ryanstapleton <megalixir.1@gmail.com>
2018-03-29 10:44:34 -0600

    Added example 2

--- basic_example_2.rb
+++ basic_example_2.rb
@@ -2,6 +2,11 @@ class String
   def censor(bad_word)
     self.gsub! "#{bad_word}", "CENSORED"
   end
+
+  def num_of_chars
+    size # existing ruby method, no need for additional self call
+  end
 end
 
 p "The bunny was in trouble with the king's bunny".censor('bunny')
+p "The bunny was in trouble with the king's bunny".num_of_chars # > 46
